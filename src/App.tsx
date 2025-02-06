276360fd4dd4902a1a1515a3722e93ba40aa63fc
Kim Fransson <kim.fransson.22@gmail.com>
2025-02-06 11:02:13 +0100

    comment unused code to trigger deploy

--- src/App.tsx
+++ src/App.tsx
@@ -27,25 +27,25 @@ import { Separator } from "./components/ui/separator";
 import { Badge } from "./components/ui/badge";
 
 import Image1 from "./assets/images/image-product-1.jpg";
-import Image1Thumbnail from "./assets/images/image-product-1-thumbnail.jpg";
+// import Image1Thumbnail from "./assets/images/image-product-1-thumbnail.jpg";
 
 import Image2 from "./assets/images/image-product-2.jpg";
-import Image2Thumbnail from "./assets/images/image-product-2-thumbnail.jpg";
+// import Image2Thumbnail from "./assets/images/image-product-2-thumbnail.jpg";
 
 import Image3 from "./assets/images/image-product-3.jpg";
-import Image3Thumbnail from "./assets/images/image-product-3-thumbnail.jpg";
+// import Image3Thumbnail from "./assets/images/image-product-3-thumbnail.jpg";
 
 import Image4 from "./assets/images/image-product-4.jpg";
-import Image4Thumbnail from "./assets/images/image-product-4-thumbnail.jpg";
+// import Image4Thumbnail from "./assets/images/image-product-4-thumbnail.jpg";
 import { AspectRatio } from "./components/ui/aspect-ratio";
 
 const images = [Image1, Image2, Image3, Image4];
-const thumbnails = [
-  Image1Thumbnail,
-  Image2Thumbnail,
-  Image3Thumbnail,
-  Image4Thumbnail,
-];
+// const thumbnails = [
+//   Image1Thumbnail,
+//   Image2Thumbnail,
+//   Image3Thumbnail,
+//   Image4Thumbnail,
+// ];
 
 const links = ["Collections", "Men", "Women", "About", "Contact"];
 
